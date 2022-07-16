3d0f039de7b2367d9bc7e8ac87594b2f3dc6fe93
Strider <dave.barnes23@gmail.com>
2022-07-15 22:33:34 -0500

    GET api is setup and part way through the POST api setup

--- routes/apiRoutes/notesRoute.js
+++ routes/apiRoutes/notesRoute.js
@@ -1,2 +1,27 @@
+
 const router = require('express').Router();
-const { db } = require('../../db/db.json');
+const { retrieveNotes, saveNotes, validateNotesTitle, validateNotesText } = require('../../lib/notes');
+const { notes } = require('../../db/db.json');
+
+router.get('/notes', (req, res) => {
+    let results = notes;
+    if (req) {
+        results = retrieveNotes(req, results);
+    }
+    res.json(results);
+});
+
+router.post('/notes', (req, res) => {
+    console.log(req);
+    if (!validateNotesTitle(req.body)) {
+        res.status(400).send('Please add a note title');
+    } else if (!validateNotesText(req.body)) {
+        res.status(400).send('Please add text to this note');
+    } else {
+        const newNote = saveNotes(req.body, notes);
+        console.log(newNote);
+        res.json(newNote);
+    }
+});
+
+module.exports = router;
